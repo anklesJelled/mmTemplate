f8e4bd2bb46fc7e16fb1ea4e935ca70632fd7e01
K <anklesjelled@gmail.com>
2017-04-19 13:17:17 -0700

    js/defer.js : add backToTop script

--- source/js/defer.js
+++ source/js/defer.js
@@ -1,2 +1,22 @@
-// ------------------------------------------------------------ Google Analytics
+// ------------------------------------------------------------Back To Top
+jQuery(document).ready(function() {
+    var offset = 500;
+    var duration = 200;
+    jQuery(window).scroll(function() {
+        if (jQuery(this).scrollTop() > offset) {
+            jQuery('#backToTop').fadeIn(duration);
+        } else {
+            jQuery('#backToTop').fadeOut(duration);
+        }
+    });
+    jQuery('#backToTop').click(function(event) {
+        event.preventDefault();
+        jQuery('html, body').animate({
+            scrollTop: 0
+        }, duration);
+        return false;
+    })
+});
+
+// ------------------------------------------------------------Google Analytics
 
